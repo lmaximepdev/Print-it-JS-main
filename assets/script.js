4e1bc476f5861c0d5f0a807b21de9acc50270c33
lmaximep <lmaximep.dev@gmail.com>
2023-06-23 19:55:32 +0200

    Projet Terminé

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,36 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let banner = document.querySelector(".banner-img")
+let text = document.querySelector("#banner p")
+let left = document.querySelector(".arrow_left")
+let right = document.querySelector(".arrow_right")
+let dot = document.querySelector(".dot")
+let position = 0
+
+const dots = document.querySelectorAll(".dot")
+
+dots[position].classList.add("dot_selected")
+
+left.addEventListener ("click", function() {
+	dots[position].classList.remove("dot_selected")
+	position--;
+	if (position < 0) {
+		position = slides.length - 1
+	}
+	dots[position].classList.add("dot_selected")
+	banner.src = "./assets/images/slideshow/"+slides[position]["image"]
+	text.innerHTML = slides[position]["tagLine"];
+})
+
+right.addEventListener ("click", function() {
+	dots[position].classList.remove("dot_selected")
+	position++;
+	if (position > slides.length - 1) {
+		position = 0
+	}
+	dots[position].classList.add("dot_selected")
+	banner.src = "./assets/images/slideshow/"+slides[position]["image"]
+	text.innerHTML = slides[position]["tagLine"];
+})
